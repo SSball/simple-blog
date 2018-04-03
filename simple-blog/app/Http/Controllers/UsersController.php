5f61316a96f51382ee0e67b40e0ef91f0445273b
SSball <superslowball11@gmail.com>
2018-04-03 15:15:57 +0900

    create users page

--- simple-blog/app/Http/Controllers/UsersController.php
+++ simple-blog/app/Http/Controllers/UsersController.php
@@ -6,10 +6,5 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
-    //..
-    public function index()
-    {
-        $users = User::where('id', '!=', auth()->user()->id)->get();
-        return view('users.index', compact('users'));
-    }
+    //
 }
